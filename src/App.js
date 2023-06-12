6e769b5d0337871384921539cea2cdc6df59cdf3
Youngju-Jang <joj1043@nate.com>
2023-06-12 20:50:51 +0900

    [Feat] useEffect
    related to #5

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
-import Button from "./Button";
-import styles from "./App.module.css";
-
+import { useState, useEffect } from "react";
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (e) => {
+    setKeyword(e.target.value);
+  };
+  console.log("I RUN EVERY TIME");
+  useEffect(() => {
+    console.log("I RUN ONLY ONCE");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("I RUN more than 5 length");
+    }
+  }, [keyword]);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Continue"} />
+      <input value={keyword} onChange={onChange} type="text" placeholder="Search here..." />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
     </div>
   );
 }
