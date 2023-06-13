3e6ec0220bb9d2031e4e4088955812c6514dfc4b
Youngju-Jang <joj1043@nate.com>
2023-06-13 15:45:38 +0900

    [Refactor] 폴더 분리 & 컴포넌트 분리 & 렌더링위한 셋팅
    Related to #6

--- src/App.js
+++ src/App.js
@@ -1,40 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (
-      await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=9&sort_by=year`)
-    ).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-  }, []);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map((m) => (
-            <div key={m.id}>
-              <img src={m.medium_cover_image} />
-              <h2>{m.title}</h2>
-              <p>{m.summary}</p>
-              <ul>
-                {m.genres.map((g) => (
-                  <li key={g}>{g}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+  return null;
 }
 
 export default App;
